d91d184d563e8be83be988e252ab8db9435904cb
sweethoneybee <jsjphone8@gmail.com>
2020-06-18 16:38:15 +0900

    Implement LRU with clock algorithm But in the problem, clock algorithm doesn't work well. Here needs precise LRU, not the approximate LRU. So, need to implement LRU with double linked list.

--- C++/LeetCode_146_LRU_Cache.cpp
+++ C++/LeetCode_146_LRU_Cache.cpp
@@ -1,5 +1,10 @@
 // https://leetcode.com/problems/lru-cache/description/#
 #include <unordered_map>
+#include <iostream>
+#include <iterator>
+#include <queue>
+using namespace std;
+
 class LRUCache
 {
 public:
@@ -23,15 +28,19 @@ public:
 
     void put(int key, int value)
     {
+        if (m.find(key) != m.end())
+            return;
         if (numberOfKeys == capacity)
         {
             int temp;
             temp = que.front();
             que.pop();
-
+            cout << "temp: " << temp << endl;
             unordered_map<int, pair<int, int>>::iterator iter;
             while ((iter = m.find(temp))->second.second == 1)
             {
+                cout << "iter->first: " << iter->first << endl;
+                cout << "iter->second.second: " << iter->second.second << endl;
                 iter->second.second = 0;
                 que.push(temp);
                 temp = que.front();
@@ -45,7 +54,7 @@ public:
         else
         {
             m.insert(make_pair(key, make_pair(value, 0)));
-            que.push(value);
+            que.push(key);
             numberOfKeys++;
         }
     }
@@ -63,3 +72,25 @@ private:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+int main(void)
+{
+    LRUCache *obj = new LRUCache(2);
+
+    cout << "put(2, 1)" << endl;
+    obj->put(2, 1);
+
+    cout << "put(2, 2)" << endl;
+    obj->put(2, 2);
+
+    cout << "get(2) : " << obj->get(2) << endl;
+
+    cout << "put(3, 2)" << endl;
+    obj->put(3, 2);
+
+    // cout << "get(2) : " << obj->get(2) << endl;
+
+    // cout << "get(3) : " << obj->get(3) << endl;
+
+    system("pause");
+    return 0;
+}
